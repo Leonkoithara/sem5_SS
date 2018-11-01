fbf323901f702b9e3e1c4bc1215daaffe2de2fd7
Leonkoithara <leon.koithara@gmail.com>
2018-11-01 17:45:48 +0530

    Completed dining philopher

--- Assembler/passI.c
+++ Assembler/passI.c
@@ -1,16 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void main()
+int main()
 { 
 	FILE *fp;
-	char label[10], opcode[10], operand[10];
+	char label[10], opcode[10], operand[10], c;
+	char empty[] = "\t";
 
 	int locctr, i;
 
 	fp = fopen("assembly.txt", "r");
 
-	while(fscanf(fp, "%s", opcode) >= )
+	while((c = fgetc(fp)) != EOF)
 	{ 
+		i = 0;
+		if(c != '\t')
+		{
+			while(c != '\t')
+			{ 
+				label[i] = c;
+				i++;
+				c = fgetc(fp);
+			}
+			label[i] = '\0';
+		}
+		i = 0;
+		while(c == '\t')
+			c = fgetc(fp);
+		while(c != '\t')
+		{ 
+			opcode[i] = c;
+			i++;
+			c = fgetc(fp);
+		}
+		opcode[i] = '\0';
+
+		i = 0;
+		while(c == '\t')
+			c = fgetc(fp);
+ 
+		if(c != '\n')
+		{
+			while(c != '\n')
+			{ 
+				operand[i] = c;
+				i++;
+				c = fgetc(fp);
+			}
+		}
+		operand[i] = '\0';
+
+		printf("%s\t%s\t%s\n", label, opcode, operand);
+
+		label[0] = '\0';
 	}
 }
--- dining_philosphers.c
+++ dining_philosphers.c
@@ -2,77 +2,75 @@
 #include <semaphore.h>  
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
- 
+
 enum States { THINKING, HUNGRY, EATING };  
- 
+
 sem_t chopsticks[5];
 enum States state[5];
 enum States prev_state[5];
-char *names[] = {"Aristole", "Pythogorus", "Archimedes", "Plato", "Socrates"};
+char names[5][20] = {"Aristole", "Pythogorus", "Archimedes", "Plato", "Socrates"};
+clock_t t[5];
 
 void* thread(void* args)
 { 
-
-	int p;
-	int *n = args;
-	char *name = names[*n];
+	int *n = (int*)args;
 	state[*n] = THINKING;	
+	t[*n] = clock();
+	printf("%s(%ld): THINKING\n", names[*n], t[*n]);
+	sleep(1);
 
 	while(1)
-	{ 
-		p = rand()%5;
-		if(p == 0)
-			state[*n] = HUNGRY;	
+	{
 
-		if(state[*n] == HUNGRY)
-		{  
-			sem_wait(&chopsticks[(*n+1)%5]);
-			sem_wait(&chopsticks[*n]);
- 
+		t[*n] = clock();
+		printf("%s(%ld): HUNGRY\n", names[*n], t[*n]);
+		{ 
+			if((*n+1)%5>*n)
+			{ 
+				sem_wait(&chopsticks[(*n+1)%5]);
+				sem_wait(&chopsticks[(*n)%5]);
+			}
+			else
+			{ 
+				sem_wait(&chopsticks[(*n)%5]);
+				sem_wait(&chopsticks[(*n+1)%5]);
+			}
+
 			state[*n] = EATING;
-			sleep(100);
+			t[*n] = clock();
+			printf("%s(%ld): EATING\n", names[*n], t[*n]);  
+			sleep(1);
 
 			sem_post(&chopsticks[(*n+1)%5]);
 			sem_post(&chopsticks[*n]);
 
 			state[*n] = THINKING;
+			t[*n] = clock();
+			printf("%s(%ld): THINKING\n", names[*n], t[*n]);  
+			sleep(1);
+
 		}
 	}
+	return NULL;
 }
 
-int if_change()
-{ 
-	int i;
-
-	for(i = 0;i < 5;i++)
-	{ 
-		if(prev_state[i] != state[i])
-			return 1;
-	}
-	for(;i < 5;i++)
-		prev_state[i] == state[i];
-
-	return 0;
-}
- 
-void main()
+int main()
 {
 	pthread_t philosphers[5];
-	int i;
+	int i, arr[] = {0, 1, 2, 3, 4};
 
 	for(i = 0;i < 5;i++)
 		sem_init(&chopsticks[i], 0, 1);
 
-	for(i = 0;i < 5;i++) 
-		pthread_create(&philosphers[i], NULL, thread, &i);
+	for(i = 0;i < 5;i++)
+		pthread_create(&philosphers[i], NULL, thread, &arr[i]);
+	for(i = 0;i < 5;i++)
+		pthread_join(philosphers[i], NULL);
 
-	while(1)
-	{ 
-		if(if_change())
-		{ 
-			for(i = 0;i < 5;i++)
-				printf("%s : %d\n", names[i], state[i]);
-		}
-	}
+	for(i = 0;i < 5;i++)
+		sem_destroy(&chopsticks[i]);
+
+	return 0;
 }
