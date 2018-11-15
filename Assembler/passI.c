0ed5bf88d056f04b165bc6be25ad5c07b4823834
Leonkoithara <leon.koithara@gmail.com>
2018-11-16 01:05:25 +0530

    Completed one pass assembler

--- Assembler/passI.c
+++ Assembler/passI.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int start_addr, locctr;
+int start_addr; 
+int locctr = 0;
 
 void process(char*, char*, char*);
  
 int main()
 { 
-/*	FILE *fp;
-	char label[10], opcode[10], operand[10], c;
+	FILE *fp, *ifile;
+	char label[10], opcode[10], operand[10], assembler_src[20], c;
 
-	int locctr = 0, i;
+	int i;
 
-	fp = fopen("assembly.txt", "r");
+	printf("Enter source file: ");
+	scanf("%s", assembler_src);
+ 
+	fp = fopen(assembler_src, "r");
+	ifile = fopen("intermediate.txt", "w");
 
 	while((c = fgetc(fp)) != EOF)
 	{ 
@@ -32,7 +37,7 @@ int main()
 		i = 0;
 		while(c == '\t')
 			c = fgetc(fp);
-		while(c != '\t')
+		while(c != '\t' && c != '\n')
 		{ 
 			opcode[i] = c;
 			i++;
@@ -55,62 +60,64 @@ int main()
 		}
 		operand[i] = '\0';
 
-		process(label, opcode, operand, &locctr);
+		process(label, opcode, operand);
+		fprintf(ifile, "%s\t%s\t%s\n", label, opcode, operand);
 
 		label[0] = '\0';
 	}
-*/
-	char label[] = "LAB1", opcode[] = "TD", operand[] = "200";
-	process(label, opcode, operand);
-	printf("Program length: %d", locctr-start_addr);
+
+	fclose(fp);
+	fclose(ifile);
+
+	printf("Program length: %d\n", locctr-start_addr);
 }
 
 void process(char *label, char *opcode, char *operand)
 { 
 	FILE *optab, *symtab;
- 
-	char start[] = "START", zero[] = "0", ass_direc[5][20] = {"WORD", "RESW", "RESB", "BYTE"}, temp[30];
-	char op[20];
+
+	char start[] = "START", temp[30];
+   	char ass_direc[4][20] = {"WORD", "RESW", "RESB", "BYTE"}; 
+	char op_frm_tab[20];
 
 	int code, i = 0, flag = 0, temp2;
 
-	if(!strcmp(label, start) && locctr == 0)
+	if(!strcmp(opcode, start) && locctr == 0)
 	{
+		symtab = fopen("symtab.txt", "w");
 		locctr = atoi(operand);
 		start_addr = locctr;
+		fclose(symtab);
+		return;
 	}
 
-	symtab = fopen("symtab.txt", "w+");
+	symtab = fopen("symtab.txt", "r+");
 	optab = fopen("optab.txt", "r");
 
-	printf("Reach1\n");
-	while(fscanf(symtab, "%s %d", temp, &temp2))
+	while(fscanf(symtab, "%s %d", temp, &temp2) != -1)
 	{ 
-		printf("%s %d", temp, temp2);
 		if(!strcmp(temp, label))
 		{ 
 			printf("Error: Duplicate Symbol\n");
 			return;
 		}
 	}	
-	fprintf(symtab, "%s %d", label, locctr);
-
-	printf("Reach2\n");
+	fprintf(symtab, "%s %d ", label, locctr);
 
 	while(1)
 	{ 
-		fscanf(optab, "%s %d", op, &code);
+		fscanf(optab, "%s %d", op_frm_tab, &code);
 
 		if(i < 4)
 		{ 
-			if(!strcmp(ass_direc[i], op))
+			if(!strcmp(ass_direc[i], opcode))
 			{
 				flag = 1;
 				break;
 			}
 			i++;
 		}
-		if(!strcmp(opcode, op))
+		if(!strcmp(opcode, op_frm_tab))
 		{
 			locctr += 3;
 			break;
@@ -118,34 +125,21 @@ void process(char *label, char *opcode, char *operand)
 
 	}
 
-	printf("Reach3\n");
 	if(flag)
 	{ 
 		switch(i)
 		{ 
-			case 0:
+			case 0:								//WORD
 				locctr += 3;
 				break;
-			case 1:
+			case 1:								//RESW
 				locctr += 3*atoi(operand);
 				break;
-			case 2:
+			case 2:								//RESB
 				locctr += atoi(operand);
 				break;
-			case 3:
-				temp2 = atoi(operand);
-				 
-				if(!temp2 && strcmp(operand, zero))
-					locctr += strlen(operand);
-				else
-				{ 
-					while(temp2 > 0)
-					{ 
-						(locctr)++;
-						temp2 = temp2/256;
-					}
-				}
-				 
+			case 3:								//BYTE
+				locctr += strlen(operand) - 3; 
 				break;
 			default:
 				printf("Error: Invalid Operation Code\n");
